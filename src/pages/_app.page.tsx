fb4d223cb9bfb10d3cc72786c1b7595e0a956525
Matteus Varlesse <varlesse04@gmail.com>
2023-06-29 11:37:43 -0300

    trocando de repositório

---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,35 @@
+import '../lib/dayjs'
+import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
+import { globalStyles } from '../styles/global'
+import { queryClient } from '../lib/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { DefaultSeo } from 'next-seo'
+
+globalStyles()
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: 'https://ignite-call.com.br',
+            siteName: 'Ignite Call',
+          }}
+          twitter={{
+            handle: '@handle',
+            site: '@site',
+            cardType: 'summary_large_image',
+          }}
+        />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
+  )
+}
